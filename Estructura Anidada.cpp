babc271c69bd91b8d7781a3b23d9f54a5fe08648
Amidwar <amidwar05@gmail.com>
2025-08-22 07:43:10 -0500

    Estructura de empleados (Anidadas)

--- Estructura Anidada.cpp
+++ Estructura Anidada.cpp
@@ -1,6 +1,6 @@
 //Estructuras anidadas
 #include<iostream>
-usingn namespace std;
+using namespace std;
 
 struct info_direccion{
 	char direccion[20];
@@ -18,7 +18,29 @@ struct empleado{
 
 int main(){
 	
+	for(int i=0; i<2; i++){
+		cout<<"Digite su nombre: ";
+		cin.getline(empleados[i].nombre,20,'\n');
+		cout<<"Digite su direccion: ";
+		cin.getline(empleados[i].dir_empleado.direccion,20,'\n');
+		cout<<"Digite su ciudad: ";
+		cin.getline(empleados[i].dir_empleado.ciudad,20,'\n');
+		cout<<"Digite la provincia: ";
+		cin.getline(empleados[i].dir_empleado.provincia,20,'\n');
+		cout<<"Digite su salario: ";
+		cin>>empleados[i].salario; 
+		cout<<"\n";
+	}
 	
+	//imprimiendo los datos
+	
+	for(int i=0; i<2; i++){
+		cout<<"Nombre: "<<empleados[i].nombre<<endl;
+		cout<<"Direccion: "<<empleados[i].dir_empleado.direccion<<endl;
+		cout<<"Ciudad: "<<empleados[i].dir_empleado.ciudad<<endl;
+		cout<<"Provincia: "<<empleados[i].dir_empleado.provincia<<endl;
+		cout<<"Salario: "<<empleados[i].salario<<endl;
+	}
 	
 	return 0;
 }
