7528af885362986c7bb5ee94017c87d2d245a03d
Amidwar <amidwar05@gmail.com>
2025-08-21 20:30:29 -0500

    Estructuras basicas terminadas

--- Estructura Basica.cpp
+++ Estructura Basica.cpp
@@ -9,10 +9,15 @@ persona1 = {"Amidwar", 20},
 persona2 = {"Lucia", 22};
 
 int main(){
+	cout<<"______________________\n";
 	cout<<"Nombre Persona 1: "<<persona1.nombre;
-	cout<<"Edad Persona 2: "<<persona1.edad;
-	
-	
+	cout<<endl;
+	cout<<"Edad Persona 1: "<<persona1.edad;
+	cout<<endl;
+	cout<<"_______________________\n";
+	cout<<"Nombre Persona 2: "<<persona2.nombre;
+	cout<<endl;
+	cout<<"Edad Persona 2: "<<persona2.edad;
 	
 	return 0;
 }
