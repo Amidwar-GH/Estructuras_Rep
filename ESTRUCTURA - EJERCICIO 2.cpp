2b7a5d653c69cc7f83bf9c0cb27e6192dca19b0d
Amidwar <amidwar05@gmail.com>
2025-08-22 12:55:28 -0500

    ESTRCUTURAS EJERCICIO DE PROMEDIOS (VERSION FINAL)

--- ESTRUCTURA - EJERCICIO 2.cpp
+++ ESTRUCTURA - EJERCICIO 2.cpp
@@ -12,14 +12,38 @@ struct Alumno{
 	char nombre[20];
 	int edad;
 	float promedio;
-};
+}alumnos[3];
 
 
 
 int main(){
+	//int i;
+	for(int i=0; i<3; i++){
+		fflush(stdin);
+		cout<<"Nombre del alumno "<<i+1<<": "<<endl;
+		cin.getline(alumnos[i].nombre,20,'\n');
+		cout<<"Edad del alumno "<<i+1<<": "<<endl;
+		cin>>alumnos[i].edad;
+		cout<<"Promedio del alumno "<<i+1<<": "<<endl;
+		cin>>alumnos[i].promedio;
+		cout<<"______________________\n";
+	}
 	
+	float mayor = alumnos[0].promedio;
+	int posicion = 0;
 	
+	for(int i=1; i<3; i++){
+		if(alumnos[i].promedio > mayor){
+			mayor = alumnos[i].promedio;
+			posicion = i;
+		}
+	}
 	
-	
+	cout<<"\n\n___________________________\n";
+	cout<<"La nota mas alta es de: "<<mayor;
+	cout<<"\nDATOS OBTENIDOS"<<endl;
+	cout<<"Nombre: "<<alumnos[posicion].nombre<<endl;
+	cout<<"Edad: "<<alumnos[posicion].edad<<endl;
+	cout<<"Promedio: "<<alumnos[posicion].promedio;
 	return 0;
 }
