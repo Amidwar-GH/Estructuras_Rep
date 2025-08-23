f97499db1a962c9ced32178c49da8fb3df212280
Amidwar <amidwar05@gmail.com>
2025-08-23 10:47:25 -0500

    Estrucutra de empleados con salario (VERSION FINAL)

--- ESTRUCTURA - EJERCICIO 3.cpp
+++ ESTRUCTURA - EJERCICIO 3.cpp
@@ -11,6 +11,41 @@ struct Empleado{
 
 int main(){
 	
+	int n_empleados;
+	float mayor=0 , menor=9999; 
+	int posM, posm;
+	
+	cout<<"Digite el numero de empleados: ";
+	cin>>n_empleados;
+	cout<<endl;
+	for(int i=0; i<n_empleados; i++){
+		fflush(stdin);
+		cout<<i+1<<". Nombre del empleado: ";
+		cin.getline(emp[i].nombre,20,'\n');
+		cout<<i+1<<". Salario del empleado: ";
+		cin>>emp[i].salario;
+		cout<<"\n";
+		
+		if(emp[i].salario>mayor){
+			mayor = emp[i].salario;
+			posM = i;
+		}
+		if(emp[i].salario<menor){
+			menor = emp[i].salario;
+			posm = i;
+		}
+	}
+	
+	cout<<"\n.:Datos del empleado con mayor salario:.\n";
+	cout<<"Nombre: "<<emp[posM].nombre<<endl;
+	cout<<"Salario: "<<emp[posM].salario<<endl;
+	
+	cout<<"\n.:Datos del empleado con menos salario:.\n";
+	cout<<"Nombre: "<<emp[posm].nombre<<endl;
+	cout<<"Salario: "<<emp[posm].salario<<endl;
+	
+	
+	
 	
 	return 0;
 }
