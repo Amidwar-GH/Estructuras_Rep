7a6fd21f6c5a7749aa10c38df79d4a8a4d2bb7f2
Amidwar <amidwar05@gmail.com>
2025-08-24 14:47:16 -0500

    Definiendo las estrucutras de promedio y alumno

---
+++ ESTRUCTURA - EJERCICIO6.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+struct Promedio{
+	float nota1;
+	float nota2;
+	float nota3;
+};
+
+struct Alumno{
+	char sexo[20];
+	int edad;
+	char nombre[20];
+	struct Promedio prom;
+};
+
+
+int main(){
+	
+	
+	
+	return 0;
+}
