5053c2029b6355b1af89ebbbeb1f12ad787b13cf
Amidwar <amidwar05@gmail.com>
2025-08-24 19:34:18 -0500

    Definiendo la estrucutra persona con los campos de nombre y discapacidad

---
+++ ESTRUCTURA - EJERCICIO 8.cpp
@@ -0,0 +1,26 @@
+/*Defina una estructura que sirva para representar a una persona.
+La estructura debe contener dos campos:
+-el nombre de la persona
+-un valor de tipo logico
+que indica si la persona tiene algun tipo de discapacidad. Realize un programa
+que dado un vector de personas rellene dos nuevos vectores: uno que contenga
+las personas que no tienen ninguna discapacidad y otro que contenfa las personas
+con discapacidad.*/
+
+#include<iostream>
+using namespace std;
+
+struct Persona{
+	char nombre[20];
+	bool discapacidad;
+};
+
+int main(){
+	
+	
+	
+	
+	
+	
+	return 0;
+}
